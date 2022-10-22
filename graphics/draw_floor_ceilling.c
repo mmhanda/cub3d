1efe4710341e5df1cbaf59e2c1dd5b729436356f
Anas TABITI <atabiti@e1r3p1.1337.ma>
2022-10-22 10:25:59 +0100

    RGB

--- graphics/draw_floor_ceilling.c
+++ graphics/draw_floor_ceilling.c
@@ -6,13 +6,13 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 08:55:12 by atabiti           #+#    #+#             */
-/*   Updated: 2022/10/22 10:20:58 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/10/22 10:25:32 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub.h"
 
-void	draw_floor(t_parce *game_map)
+void	draw_ceilling(t_parce *game_map)
 {
 	int	y2;
 	int	x2;
@@ -20,20 +20,19 @@ void	draw_floor(t_parce *game_map)
 
 	y2 = 0;
 	x2 = 0;
-	printf( "| %d |    \n",  game_map->f_colo);
 	while (y2 < 720 / 2)
 	{
 		x2 = 0;
 		while (x2 < 1280)
 		{
-			img_pix_put(&game_map->mlx_srct.mlx_m, x2, y2, game_map->f_colo);
+			img_pix_put(&game_map->mlx_srct.mlx_m, x2, y2, game_map->c_colo);
 			x2++;
 		}
 		y2++;
 	}
 }
 
-void	draw_ceilling(t_parce *game_map)
+void	draw_floor(t_parce *game_map)
 {
 	int y2;
 	int x2;
@@ -44,7 +43,7 @@ void	draw_ceilling(t_parce *game_map)
 		x2 = 0;
 		while (x2 < 1280)
 		{
-			img_pix_put(&game_map->mlx_srct.mlx_m, x2, y2,  game_map->c_colo);
+			img_pix_put(&game_map->mlx_srct.mlx_m, x2, y2,  game_map->f_colo);
 
 			x2++;
 		}
