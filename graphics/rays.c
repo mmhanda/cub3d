76b617eb4b1cfbc63469fdadc87e2a8516d5a30c
Anas TABITI <atabiti@e1r4p1.1337.ma>
2022-10-24 10:01:40 +0100

    problem  in  texture

--- graphics/rays.c
+++ graphics/rays.c
@@ -3,13 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   rays.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhanda <mhanda@student.42.fr>              +#+  +:+       +#+        */
+/*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 04:57:23 by mhanda            #+#    #+#             */
-/*   Updated: 2022/10/24 06:04:18 by mhanda           ###   ########.fr       */
+/*   Updated: 2022/10/24 10:01:15 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "../cub.h"
 
@@ -43,39 +42,39 @@ void	calculate_distances(t_mlx *mlx_srct, t_parce *game)
 	correct_distance = mlx_srct->hited.distance_to_wall
 		* cos(mlx_srct->rays.ray_angle - mlx_srct->plyr.rotate);
 	// mlx_srct->hited.distance_to_wall = correct_distance;
-
-
-	
-	mlx_srct->hited.distbtwplr_and_plane = ((WIDTH / 2) / tan(FOV_ANGLE / 2.00));
-
-
-
-
-	
-	mlx_srct->hited.projectedWallHeight = (int)((TILE_SIZE/ correct_distance)* mlx_srct->hited.distbtwplr_and_plane);
-
-
-
-
-	
-	mlx_srct->hited.bottomOfWall = ((HEIGHT / 2) + (mlx_srct->hited.projectedWallHeight) / 2);
-
-
-
-			
-	mlx_srct->hited.topOfWall = ((HEIGHT / 2) 	- mlx_srct->hited.projectedWallHeight / 2);
+	mlx_srct->hited.distbtwplr_and_plane = ((WIDTH / 2) / tan(FOV_ANGLE
+				/ 2.00));
+	mlx_srct->hited.projectedWallHeight = (int)((TILE_SIZE / correct_distance)
+			* mlx_srct->hited.distbtwplr_and_plane);
+	mlx_srct->hited.bottomOfWall = ((HEIGHT / 2)
+			+ (mlx_srct->hited.projectedWallHeight) / 2);
+	mlx_srct->hited.topOfWall = ((HEIGHT / 2)
+			- mlx_srct->hited.projectedWallHeight / 2);
 	if (mlx_srct->hited.topOfWall < 0 || mlx_srct->hited.topOfWall > HEIGHT)
 		mlx_srct->hited.topOfWall = 0;
 	if (mlx_srct->hited.bottomOfWall < 0 || mlx_srct->hited.topOfWall > HEIGHT)
 		mlx_srct->hited.topOfWall = HEIGHT;
-	
-	
 }
 void	paint_rays(t_mlx *mlx_srct, t_parce *game)
 {
-	double	column_id;
+	int	column_id;
 	int		color;
+/********** texture **********/
+t_img img;
+img.endian = 0;
+// img.bpp = 32;
+// img.size_line = WIDTH;
+//read xpm file 
+int h = 32;
+int *h_ptr = & h;
+//  img.addr =   mlx_get_data_addr( mlx_xpm_file_to_image(mlx_srct->mlx_ptr,"./textur/walld.xpm",h_ptr, h_ptr),&img.bpp,&img.size_line, &img.endian);
+// void *img_xpm = mlx_xpm_file_to_image(mlx_srct->mlx_ptr,"./textur/wall2.xpm",h_ptr, h_ptr);
+void *img_xpm = mlx_xpm_file_to_image(mlx_srct->mlx_ptr,"./textur/wall2.xpm",h_ptr, h_ptr);
+img.addr= mlx_get_data_addr(img_xpm,&img.bpp, &img.size_line, &img.endian);
+// printf("color  =  %x\n ",(int  )img.addr +1);
 
+/****************************/
+	
 	init_them(mlx_srct);
 	mlx_srct->hited.distbtwplr_and_plane = 0;
 	column_id = 0;
@@ -93,10 +92,25 @@ void	paint_rays(t_mlx *mlx_srct, t_parce *game)
 		mlx_srct->hited.hiity = 0;
 		mlx_srct->hited.distance_to_wall = 0;
 		put_rays(mlx_srct, mlx_srct->plyr.x, mlx_srct->plyr.y, game);
-		if (mlx_srct->hited.wasverticallasttime == true)
-			color = game->f_colo + 0x3916E6;
+		// if (mlx_srct->hited.wasverticallasttime == true)
+		// 	color = game->f_colo + 0x3916E6;
 		calculate_distances(mlx_srct, game);
-		draw_it(column_id * WALL_STRIP_THIKNES, mlx_srct->hited.topOfWall, 1,(mlx_srct->hited.bottomOfWall) + 1, color, mlx_srct);
+		int	ff;
+		int *c;
+		int iter = 0;
+	// c =  (int*)(img.addr + (iter));
+	ff =  mlx_srct->hited.topOfWall;
+	int yy = 0;
+	while (ff < (mlx_srct->hited.bottomOfWall) + 1)
+	{
+			c =  (int*)(img.addr + ((yy  % 32) * column_id));
+		img_pix_put(&mlx_srct->mlx_m, column_id, ff, *c);
+		yy++;
+		ff++;
+
+	}
+	printf(" (int ) (img.addr + (0) = %d \n",  color);
+		// draw_it(column_id * WALL_STRIP_THIKNES, mlx_srct->hited.topOfWall, 1,(mlx_srct->hited.bottomOfWall) + 1, color, mlx_srct);
 		column_id++;
 		mlx_srct->rays.ray_angle += (FOV_ANGLE / WIDTH);
 	}
