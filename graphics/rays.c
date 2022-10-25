c0764fac2cb6ae930e2fa43f3b1d1480a0e2abc4
Anas TABITI <atabiti@e1r4p1.1337.ma>
2022-10-25 15:15:51 +0100

    g

--- graphics/rays.c
+++ graphics/rays.c
@@ -6,7 +6,7 @@
 /*   By: atabiti <atabiti@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 04:57:23 by mhanda            #+#    #+#             */
-/*   Updated: 2022/10/25 15:14:36 by atabiti          ###   ########.fr       */
+/*   Updated: 2022/10/25 15:15:26 by atabiti          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,24 +99,18 @@ void	get_right_pixel(t_mlx *mlx_srct, t_parce *game, int ofssety)
 {
 	if ((!is_down(mlx_srct->rays.ray_angle))
 		&& mlx_srct->hited.wasverticallasttime == false)
-	{
 		mlx_srct->hited.color = mlx_srct->hited.data_so + ((64 * ofssety)
 				+ mlx_srct->hited.offset);
-	}
 	else if ((!is_down(mlx_srct->rays.ray_angle))
 			&& mlx_srct->hited.wasverticallasttime == true
 			&& is_right(mlx_srct->rays.ray_angle))
-	{
 		mlx_srct->hited.color = mlx_srct->hited.data_we + ((64 * ofssety)
 				+ mlx_srct->hited.offset);
-	}
 	else if ((!is_down(mlx_srct->rays.ray_angle))
 			&& mlx_srct->hited.wasverticallasttime == true
 			&& !is_right(mlx_srct->rays.ray_angle))
-	{
 		mlx_srct->hited.color = mlx_srct->hited.data_ea + ((64 * ofssety)
 				+ mlx_srct->hited.offset);
-	}
 	else
 		get_right_pixel_p2(mlx_srct, game, ofssety);
 }
